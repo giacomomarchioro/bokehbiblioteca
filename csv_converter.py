1fe1c0206337ad1b0913fc82d919872b33545aba
giacomounivr <giacomo.marchioro@univr.it>
2021-04-20 18:22:14 +0200

    Change converting process

--- csv_converter.py
+++ csv_converter.py
@@ -30,7 +30,19 @@ def color_binding(row):
        return '#FF5733'   
    else:
         return '#888483'
+
 catalog["color_rileg"] = catalog.apply (lambda row: color_binding(row), axis=1)
+#catalog = catalog.replace({'preferred_manifest_url': {0:"/"}})
+#catalog = catalog.replace({'preferred_manifest_url': {0:"/"}})
+catalog.preferred_manifest_url.replace([0],"/",inplace=True)
+
+def is_digitized(row):
+   if row['preferred_manifest_url'] == "/":
+        return 'Non digitalizzato'
+   else:
+        return '✓'
+
+catalog["is_digitized"] = catalog.apply (lambda row: is_digitized(row), axis=1)
 
 mdim = np.log(1 + (catalog.fogli/catalog.fogli.max())*1000)
 mdim[mdim == 0] = 4
